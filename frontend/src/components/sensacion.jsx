4c97b6e388341592ea852164092d1a33046a996e
AnaDuarte18 <apduarte@escuelasproa.edu.ar>
2026-06-24 15:30:26 -0300

    temperatura, humedad y sensacion termica se muestran, calculos de promedio por dia y semanas

---
+++ frontend/src/components/sensacion.jsx
@@ -0,0 +1,9 @@
+function IndiceDeCalor({indiceDeCalor}) {
+
+    return(
+        <div>
+            <h1>Sensación Térmica: {indiceDeCalor} °C</h1>
+        </div>
+    )
+}
+export default IndiceDeCalor;
---
+++ frontend/src/components/temperatura.jsx
@@ -0,0 +1,9 @@
+function Temperatura({temperatura}) {
+
+    return(
+        <div>
+            <h1>Temperatura: {temperatura} °C</h1>
+        </div>
+    )
+}
+export default Temperatura;
